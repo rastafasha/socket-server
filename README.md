5c31ad03e8255e408d7ac6856243192baaeee04a
Malcolm Cordova <mercadocreativo@gmail.com>
2020-04-12 01:18:45 -0400

    primer commit RestServer

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+# Socket-Server
+
+Reconstruir modulos de Node
+
+```
+npm install
+
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar servidor
+```
+nodemon (instalar en root npm install -g nodemon)
+dentro de la carpeta correr:
+
+nodemon dist/ ó
+
+node dist/
+
+
+```
---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from "./clases/server";
+import router from "./routes/router";
+import { SERVER_PORT } from './global/environment';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+ const server = new Server();
+
+ //Body Parser
+ server.app.use( bodyParser.urlencoded({extended:true}) );
+ server.app.use( bodyParser.json() );
+
+
+//CORS
+server.app.use( cors({ origin:true, credentials: true }));//permite a cualquier persona llamar los servicios
+
+ //Rutas de Servicio
+ server.app.use('/', router)
+
+server.start( ()=>{
+    console.log(`Servidor corriendo en el puerto ${SERVER_PORT}`);
+});
